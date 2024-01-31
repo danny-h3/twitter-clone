d49c49d24605fd3518adb3fde01f62c764fe6bc0
Daniel H <danielzh334@gmail.com>
2024-01-30 22:37:06 -0500

    Start of Sidebar

--- app/src/app/main/main.module.ts
+++ app/src/app/main/main.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { LayoutComponent } from './layout/layout.component';
-
+import { SidebarComponent } from './shared/components/sidebar.component';
+import { SidebaritemComponent } from './shared/components/sidebaritem.component';
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    SidebarComponent,
+    SidebaritemComponent,
   ],
   imports: [
     CommonModule,
---
+++ app/src/app/main/shared/components/sidebaritem.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebaritem',
+  templateUrl: './sidebaritem.component.html',
+  styleUrl: './sidebaritem.component.scss'
+})
+export class SidebaritemComponent {
+
+}
