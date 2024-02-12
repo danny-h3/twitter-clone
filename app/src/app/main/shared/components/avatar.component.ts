a7256dcc658a407077a39de3fbc16e78d92e2526
Daniel H <danielzh334@gmail.com>
2024-02-12 12:40:04 -0500

    Updates to Avatar and Follow Bar

--- app/src/app/main/shared/components/avatar.component.ts
+++ app/src/app/main/shared/components/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'Avatar',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
-
-}
+  @Input() userId: string = "12340";
+  @Input() isLarge: boolean = false; 
+  @Input() hasBorder: boolean = false; 
+} 
