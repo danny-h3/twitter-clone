d6d2108391c9dcbdaef5035e64e23594eabc0e97
Daniel H <danielzh334@gmail.com>
2024-02-13 19:31:58 -0500

    Starting Content Bar

--- app/src/app/main/main.module.ts
+++ app/src/app/main/main.module.ts
@@ -5,12 +5,14 @@ import { MainRoutingModule } from './main-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { SidebarbuttonsComponent } from './shared/components/sidebar/sidebarbuttons.component';
+import { ContentbarComponent } from './shared/components/contentbar/contentbar.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
     SidebarComponent,
     SidebarbuttonsComponent,
+    ContentbarComponent,
   ],
   imports: [
     CommonModule,
---
+++ app/src/app/main/shared/components/contentbar/contentbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contentbar',
+  templateUrl: './contentbar.component.html',
+  styleUrl: './contentbar.component.scss'
+})
+export class ContentbarComponent {
+
+}
